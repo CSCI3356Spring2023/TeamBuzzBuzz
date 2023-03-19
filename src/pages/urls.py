fc1147bd2fbbeee0a2d8e6e2d38e1b9eb6cda5f0
Andy Deng <denga@bc.edu>
2023-03-19 11:49:24 -0400

    fixed static/css/main.css not loading properly

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('landing_page/', views.landing_page, name='landing_page'),
-    path('course_view/', views.course_view, name='course_view'),
-    path('apply/', views.apply, name='apply'),
+    # path('', views.home, name='home'),
+    # path('landing_page/', views.landing_page, name='landing_page'),
+    # path('course_view/', views.course_view, name='course_view'),
+    # path('apply/', views.apply, name='apply'),
 ]
