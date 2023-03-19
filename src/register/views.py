beecf1323ce0a2b5be0948292868be7df5e3de1a
Andy Deng <denga@bc.edu>
2023-03-19 13:20:34 -0400

    make naming more consistent

--- src/register/views.py
+++ src/register/views.py
@@ -3,4 +3,4 @@ from django.contrib.auth.forms import UserCreationForm
 
 def register_view(request):
     form = UserCreationForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, 'register/register.html', {'form': form})
