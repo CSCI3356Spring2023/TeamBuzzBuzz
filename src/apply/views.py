7942ddf17fb47e5e83133fbe23c9fac92187e27d
Gabriella Piacentino <piacentg@bc.edu>
2023-03-19 17:50:23 -0400

    Adding apply app part 2

--- src/apply/views.py
+++ src/apply/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Apply
+from .forms import ApplicationForm
 # Create your views here.
+def apply_form_view(request):
+	form = ApplicationForm(request.POST or None) #register the object whenever it gets a request 
+	if form.is_valid():
+		form.save()
+		form = ApplicationForm()
+
+	context = {
+			'form': form
+	}
+	return render(request, 'apply.html', context)
