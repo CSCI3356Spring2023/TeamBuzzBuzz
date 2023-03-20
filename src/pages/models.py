6faf6fff09bd75047778ac8fac399f7f6861a0f1
Liam Wilson <wilsonzq@bc.edu>
2023-03-20 13:18:08 -0400

    rectifying version issues v2 - with gabi's apply update

--- src/pages/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Apply(models.Model):
-    title = models.TextField()#connect to course creation 
-    positions_open = models.IntegerField()
-    expected_hours = models.IntegerField()
-    description = models.TextField()
-    additional_information = models.TextField(blank=True,null=True) #don't need to have additional information
