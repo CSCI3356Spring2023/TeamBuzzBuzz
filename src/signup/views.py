505004cc565d3771814ff40e8f227a5df265a72f
k3vkoh <kev33koh@gmail.com>
2023-03-19 22:17:57 -0400

    signup forms and models connected and ready for django admin

--- src/signup/views.py
+++ src/signup/views.py
@@ -4,17 +4,26 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 # Create your views here.
+
 def signup_view(request, *args, **kwargs):
-	if request.method == 'POST':
-		form = SignUpForm(request.POST)
-		if form.is_valid():
-			form.save() # This saves the user to the database
-			username = form.cleaned_data.get('username')
-			messages.success(request, f'Account created for {username}!')
-			return redirect('home')
-	else:
-		form = SignUpForm()
-	context = { 'form': form }
+	my_form = SignUpForm()
+	context = {
+		'form': my_form
+	}
 	return render(request, 'signup/signup.html', context )
+
+
+# def signup_view(request, *args, **kwargs):
+# 	if request.method == 'POST':
+# 		form = SignUpForm(request.POST)
+# 		if form.is_valid():
+# 			form.save() # This saves the user to the database
+# 			username = form.cleaned_data.get('username')
+# 			messages.success(request, f'Account created for {username}!')
+# 			return redirect('home')
+# 	else:
+# 		form = SignUpForm()
+# 	context = { 'form': form }
+# 	return render(request, 'signup/signup.html', context )
 
 
