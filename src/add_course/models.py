0f2452f5a144e9ef7c0f674a954cbcf9b70cfae3
Andy Deng <denga@bc.edu>
2023-04-03 13:02:19 -0400

    pulling

--- src/add_course/models.py
+++ src/add_course/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from users.models import CustomUser as User
+from django.urls import reverse
 
 class Course(models.Model):
     OPTIONS = (
@@ -16,3 +18,11 @@ class Course(models.Model):
     discussion = models.CharField(max_length=100, choices=DISCUSSION)
     ta_required = models.CharField(max_length=100, choices=OPTIONS)
     description = models.CharField(max_length=100)
+    
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.course_title
+    
+    # def absolute_url(self):
+    #     return reverse('course_detail', kwargs={'pk': self.pk})
--- src/apply/models.py
+++ src/apply/models.py
@@ -4,10 +4,4 @@ from users.models import CustomUser as User
 
 class ApplyModel(models.Model):
     additional_information = models.TextField(blank=True,null=True)
-    course_title = models.CharField(max_length=100)
-    email = models.EmailField()
-    discussion = models.CharField(max_length=100)
-    ta_required = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
-    additional_information = models.TextField(blank=True,null=True)
 
