5b871b684f11d0e70377df001bac042b360ca9af
Sammy2023 <55414520+Sammy2023@users.noreply.github.com>
2023-04-03 15:09:23 -0400

    add offer detail

--- src/offers/views.py
+++ src/offers/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from add_course.models import Course
+from users.models import CustomUser
+from django.contrib.auth.decorators import login_required
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.exceptions import ObjectDoesNotExist
+
 
 # Create your views here.
+class OfferListView(LoginRequiredMixin, ListView):
+    model = Offer
+    template_name = 'offer/offer_list.html'
+    ordering = ['time_stamp']
+    context_object_name = 'offer_list'
+    
+    
