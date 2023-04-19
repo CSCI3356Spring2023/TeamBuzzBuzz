486e09862be8f83dec9f0cb0f2dafded5d1db172
k3vkoh <kev33koh@gmail.com>
2023-04-19 16:37:31 -0400

    add old courses

--- src/add_oldcourse/models.py
+++ src/add_oldcourse/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from users.models import CustomUser as User
+from django.urls import reverse
 
-# Create your models here.
+class Course(models.Model):
+    SEMESTER = [
+        ('FALL', 'Fall'),
+        ('Spring', 'Spring'),
+    ]
+    course_title = models.CharField(max_length=100)
+    year = models.CharField(max_length=100)
+    semester = models.CharField(max_length=100, choices=SEMESTER)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None, related_name='added_old_courses')
+
+    def __str__(self):
+        return self.course_title
+    
+    def get_absolute_url(self):
+        return reverse('home')
--- src/add_oldcourse/views.py
+++ src/add_oldcourse/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Course
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView, 
+    DeleteView
+    )
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
 
-# Create your views here.
+class CourseCreateView(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Course
+    template_name = 'add_oldcourse/add_oldcourse.html'
+    fields = ['course_title', 'year', 'semester']
+    success_url = '/'
+    success_message = "Course added successfully"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        return self.request.user.is_staff
