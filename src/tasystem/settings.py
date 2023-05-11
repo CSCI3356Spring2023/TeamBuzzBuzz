360329c6caaeee965b9063d5e6484fd0fa5335af
k3vkoh <kev33koh@gmail.com>
2023-05-11 04:30:32 -0400

    moved

--- src/tasystem/settings.py
+++ src/tasystem/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'src', 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'src', 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = 'home'
 
